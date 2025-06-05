62ec43b5478328480d141061fcdd5b86488e235e
Jefferson <jeffersonbk15@gmail.com>
2025-06-05 20:17:53 -0300

    fourth commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Efeito de carregamento suave
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.style.opacity = "1";
+document.body.style.opacity = "1";
+});
+// Efeito de hover dinâmico nos botões
+document.querySelectorAll('.botao-link').forEach(botao => {
+botao.addEventListener('mouseenter', () => {
+botao.style.transform = 'scale(1.05)';
+});
+botao.addEventListener('mouseleave', () => {
+botao.style.transform = 'scale(1)';
+});
 });
